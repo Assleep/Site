1fadafee0021c901acee5f963049617cf8f73ad6
Ilia Saiushev <sayushevilya16@gmail.com>
2019-06-30 17:32:48 +0300

    v1.0.0

--- search.js
+++ search.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  
+	$('label').click(function(){
+
+	});
+  $('#container').click(function(){
+  	$('.navigate').addClass('hide');
+  });
   $(".fa-search").click(function(){
     $(".container1, .input, .fa-search").toggleClass("active");
     $("input[type='text']").focus();
